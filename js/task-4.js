8e565b03b4b003e4b441487cd40a6bb901f65364
Novytska12321 <novytska@seznam.cz>
2024-07-25 23:42:43 +0200

    add task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const formLog = document.querySelector(".login-form");
+formLog.addEventListener("submit", (event) => {
+    event.preventDefault();
+const email = formLog.elements.email.value.trim();
+const password = formLog.elements.password.value.trim();
+
+if (email === "" || password === "") {
+    alert('All form fields must be filled in')
+} else {
+    const formData = {
+        email: email,
+        password: password,
+    };
+    console.log(formData);
+    formLog.reset();
+}
+});
+
 
