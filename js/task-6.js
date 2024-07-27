590c0fac46a8d56be4bcd5436b48e44ac73d560b
Novytska12321 <novytska@seznam.cz>
2024-07-27 21:55:28 +0200

    add task 5 and 6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const controls = document.querySelector("#controls");
+const input = document.querySelector("input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+createButton.addEventListener("click", () => {
+  const amount = Number(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  }
+  else {
+    alert('Please enter a number between 1 and 100');
+  }
+  input.value = "";
+});
+destroyButton.addEventListener("click", destroyBoxes);
+function createBoxes(amount) {
+  destroyBoxes();
+  const boxes = [];
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    const size = 30 + i * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+  boxesContainer.append(...boxes);
+}
+function destroyBoxes() {
+  boxesContainer.innerHTML = " ";
+  
+}
